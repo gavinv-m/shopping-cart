d8c34c378b9284696413350a3d4084b2144a2f0d
Gavin <gavinvm@protonmail.com>
2024-11-22 17:51:30 +0200

    feat: Fetch data on load

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { FilmsProvider } from './titles.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <FilmsProvider>
+      <App />
+    </FilmsProvider>
   </StrictMode>,
-)
+);
---
+++ src/titles.jsx
@@ -0,0 +1,71 @@
+import { createContext, useContext, useEffect, useRef, useState } from 'react';
+import getTrailerSrc from './trailer';
+
+const FilmsContext = createContext();
+
+const getTitles = () => {
+  return [
+    { title: 'Vertigo', id: 426 },
+    { title: 'The Shining', id: 694 },
+    { title: 'The Silence of the Lambs', id: 274 },
+    { title: 'The Thing', id: 1091 },
+    { title: 'Little Miss Shine', id: 773 },
+    { title: '2001: A Space Odyssey', id: 62 },
+    { title: 'Pulp Fiction', id: 680 },
+    { title: 'Badlands', id: 3133 },
+    { title: 'Jaws', id: 578 },
+    { title: 'Apocalypse Now', id: 28 },
+    { title: 'Beau Travail', id: 14626 },
+    { title: 'The Graduate', id: 37247 },
+  ];
+};
+
+const filmsPromise = (async () => {
+  const promises = getTitles().map(async (film) => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${film.id}?api_key=b2e8b9fd7484d2460a372d9ccfcb104c&append_to_response=videos`,
+        { mode: 'cors' },
+      );
+      const data = await response.json();
+      const posterPath = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
+      const videoSrc = getTrailerSrc(data);
+
+      return {
+        ...film,
+        imgSrc: posterPath,
+        vidSrc: videoSrc,
+      };
+    } catch (error) {
+      console.error(`Error fetching poster for ${film.id}:`, error);
+      return { ...film, imgSrc: null, vidSrc: null };
+    }
+  });
+
+  return await Promise.all(promises);
+})(); // IIFE
+
+export function FilmsProvider({ children }) {
+  const [films, setFilms] = useState([]);
+  const fetchedRef = useRef(false);
+
+  useEffect(() => {
+    if (fetchedRef.current) return; // Prevent double state updates
+    fetchedRef.current = true;
+
+    const fetchData = async () => {
+      const filmsDetails = await filmsPromise;
+      setFilms(filmsDetails);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <FilmsContext.Provider value={films}>{children}</FilmsContext.Provider>
+  );
+}
+
+export function useFilms() {
+  return useContext(FilmsContext);
+}
